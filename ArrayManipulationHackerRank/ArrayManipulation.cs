f99da6e9bd3fc01fbc0f93a966e362b3df6cf10a
shamim1111 <shahazada8250@yahoo.co.uk>
2020-06-17 22:09:48 +0100

    Adding the project for solving the  array manipulation at hacker rank

---
+++ ArrayManipulationHackerRank/ArrayManipulation.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArrayManipulationHackerRank
+{
+    public class ArrayManipulation
+    {
+        public long GetMaximuSumValue(long[] resultingArr)
+        {
+            long maximumSumValue = int.MinValue;
+            long sum = 0;
+            for (int i = 0; i < resultingArr.Length; i++)
+            {
+                sum += resultingArr[i];
+                maximumSumValue = Math.Max(maximumSumValue, sum);
+
+            }
+            return maximumSumValue;
+        }
+        public long ProcessArrayManipulation(int n, int[][] arr)
+        {
+            int a, b, k = 0;
+           
+            long[] resultingArr = new long[n + 2];
+            for (int i = 0; i < arr.Length; i++)
+            {
+                a = arr[i][0];
+                b = arr[i][1];
+                k = arr[i][2];
+                resultingArr[a] = resultingArr[a] + k;
+                resultingArr[b + 1] = resultingArr[b + 1] - k;
+
+             
+
+            }
+           
+
+            return GetMaximuSumValue(resultingArr);
+        }
+    }
+}
---
+++ ArrayManipulationHackerRank/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArrayManipulationHackerRank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //input based on the hacker rank input
+            int[][] arrManipulation = new int[][]
+            {
+                new int[] {1, 2, 100 },
+                new int[] { 2, 5, 100},
+                    new int[] { 3, 4, 100}
+
+            };
+
+            ArrayManipulation arm = new ArrayManipulation();
+            long value = arm.ProcessArrayManipulation(5, arrManipulation);
+            Console.WriteLine($"Max value:{value}");
+            Console.ReadKey();
+        }
+    }
+}
